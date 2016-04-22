0183f21900d41d831c3231b9e882585fd91d8d79
Derek Andre <dandre@keyholesoftware.com>
2016-04-21 21:23:18 -0500

    Initial Commit

---
+++ face-it/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  face-it
+//
+//  Created by Derek Andre on 4/21/16.
+//  Copyright © 2016 Derek Andre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
